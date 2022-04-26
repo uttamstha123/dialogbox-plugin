05e5618081af96dcf0f2731e2dd7e76478361e1e
Uttam Shrestha <61283364+uttamstha123@users.noreply.github.com>
2022-04-26 21:33:46 +0530

    Add files via upload

--- dialogbox.js
+++ dialogbox.js
@@ -1,26 +1,43 @@
-const dialogBox = document.querySelector(".dialog-box-icon");
+const dialogBoxIcon = document.querySelector(".dialog-box-icon");
 const dialogBoxForm = document.querySelector(".dialog-box-form");
 
 // pop the dialog form when icon is clicked
-dialogBox.addEventListener("click", dialogForm);
+dialogBoxIcon.addEventListener("click", dialogForm);
 
 // function defined
 function dialogForm() {
   // hide the icon
-  dialogBox.style = "display: none";
+  dialogBoxIcon.style = "display: none";
   // show the form
   dialogBoxForm.style.display = "flex";
 
+  // creating close button to the design
+  const closeIcon = document.createElement("input");
+  closeIcon.setAttribute("type", "reset");
+  closeIcon.setAttribute("id", "close");
+  closeIcon.setAttribute("value", "X");
+  dialogBoxForm.appendChild(closeIcon);
+
+  // handling close action
+  closeIcon.addEventListener("click", () => {
+    // show the icon
+    dialogBoxIcon.style = "display: grid";
+    // hide the form
+    dialogBoxForm.style.display = "none";
+  });
+
   // handle form submit
-  const submitButton = document.getElementById("submit");
+  const submitButton = document.querySelector(
+    ".dialog-box-form input[type='submit']"
+  );
   submitButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-    const email = document.getElementById("email").value;
+    document.querySelectorAll(".dialog-box-form input").forEach((input) => {
+      // console the values
+      if (input.type == "submit" || input.type == "reset") return;
 
-    // console the values
-    let consoleLog = `First Name: ${firstName} \nLast Name: ${lastName}\nEmail: ${email}`;
-    console.log(consoleLog);
+      // Only display the user input
+      console.log(input.value, "\n");
+    });
   });
 }
